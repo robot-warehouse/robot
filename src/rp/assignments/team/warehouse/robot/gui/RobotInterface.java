6717cf7970a4a304096cb096ada0414616298ace
oxu711 <oxu711@cs.bham.ac.uk>
2018-03-19 22:36:50 +0000

    Added timer to remind user

--- src/rp/assignments/team/warehouse/robot/gui/RobotInterface.java
+++ src/rp/assignments/team/warehouse/robot/gui/RobotInterface.java
@@ -2,46 +2,66 @@ package rp.assignments.team.warehouse.robot.gui;
 
 import lejos.nxt.Button;
 import lejos.nxt.LCD;
+import lejos.nxt.Sound;
+import lejos.util.Timer;
+import lejos.util.TimerListener;
 
 public class RobotInterface implements IRobotInterface {
 
-    @Override
-    public void pickUpAmountInLocation(int amount) {
+	@Override
+	public void pickUpAmountInLocation(int amount) {
 
-        assert amount >= 0;
+		assert amount >= 0;
 
-        int currentlyPickedUp = 0;
+		int currentlyPickedUp = 0;
 
-        while (currentlyPickedUp != amount) {
-            LCD.clear();
-            System.out.println("Pick up items");
-            System.out.println("Needed: " + amount);
-            System.out.println("Holding: " + currentlyPickedUp);
-            System.out.println("< - Drop off");
-            System.out.println("Pickup - >");
+		while (currentlyPickedUp != amount) {
 
-            int buttonID = Button.waitForAnyPress();
+			LCD.clear();
+			System.out.println("Pick up items");
+			System.out.println("Needed: " + amount);
+			System.out.println("Holding: " + currentlyPickedUp);
+			System.out.println("< - Drop off");
+			System.out.println("Pickup - >");
 
-            switch (buttonID) {
-                case Button.ID_LEFT:
-                    if (currentlyPickedUp != 0) {
-                        currentlyPickedUp--;
-                    }
-                    break;
-                case Button.ID_RIGHT:
-                    currentlyPickedUp++;
-                    break;
-            }
-        }
-    }
+			int theDelay = 10000; // Waits 10 seconds for user
+			TimerListener timerListener = new TimerListener() { // Plays 2 beeps
 
-    @Override
-    public void dropOffAmountInLocation(int amount) {
-        int buttonID = -1;
+				@Override
+				public void timedOut() {
+					Sound.twoBeeps();
+				}
 
-        System.out.println("Press ENTER button to drop off items");
-        while (buttonID != Button.ID_ENTER) {
-            buttonID = Button.waitForAnyPress();
-        }
-    }
+			};
+
+			Timer timer = new Timer(theDelay, timerListener);
+
+			timer.start(); // Start timer
+
+			int buttonID = Button.waitForAnyPress();
+
+			switch (buttonID) {
+			case Button.ID_LEFT:
+				timer.stop(); // Stops timer
+				if (currentlyPickedUp != 0) {
+					currentlyPickedUp--;
+				}
+				break;
+			case Button.ID_RIGHT:
+				timer.stop(); // Stops timer
+				currentlyPickedUp++;
+				break;
+			}
+		}
+	}
+
+	@Override
+	public void dropOffAmountInLocation(int amount) {
+		int buttonID = -1;
+
+		System.out.println("Press ENTER button to drop off items");
+		while (buttonID != Button.ID_ENTER) {
+			buttonID = Button.waitForAnyPress();
+		}
+	}
 }
